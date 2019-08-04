c7b049a1ca49ef9577a925e2f579676d03153f14
momolin78 <momolin78@gmail.com>
2019-08-05 07:37:22 +0900

    adding comments

--- api.py
+++ api.py
@@ -5,23 +5,20 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 db_connection = create_engine('sqlite:////Users/momol/SideProjects/sailor-moon-api/sailormoon.db') #database url
 
+#endpoint for mainpage
 @app.route("/", methods=['GET'])
 def mainPage():
-    """
-        main page
-    """
     return "Sailor Moon API"
 
+#endpoint to get all senshi 
 @app.route("/senshi/all", methods=['GET'])
 def get_all_senshi():
-    """
-        returns all senshies
-    """
     conn = db_connection.connect() # connect to database
     query = conn.execute("SELECT * FROM Senshi") #executing query
     results = {'data':[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]} #parsing through query
     return jsonify(results)
 
+#endpoint to get a senshi with a given id
 @app.route("/senshi/<int:id>", methods=['GET'])
 def find_senshi(id):
     """
