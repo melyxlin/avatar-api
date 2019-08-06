aa5460e62525a5ec22747150a945c2bb5850e1a2
momolin78 <momolin78@gmail.com>
2019-08-07 06:38:09 +0900

    reworked api

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-db_connection = create_engine('sqlite:////Users/momol/SideProjects/sailor-moon-api/sailormoon.db') #database url
+db_connection = create_engine('sqlite:////Users/momol/SideProjects/mcu-api/mcu.db') #database url
 
 
 #endpoint for mainpage
@@ -12,21 +12,46 @@ db_connection = create_engine('sqlite:////Users/momol/SideProjects/sailor-moon-a
 def mainPage():
     return "Sailor Moon API"
 
-#endpoint to get all senshi 
-@app.route("/senshi/all", methods=['GET'])
-def get_all_senshi():
+#endpoint to get all superheroes
+@app.route("/superheroes/all", methods=['GET'])
+def get_all_superheroes():
     conn = db_connection.connect() # connect to database
-    query = conn.execute("SELECT * FROM Senshi") #executing query
+    query = conn.execute("SELECT * FROM superheroes") #executing query
     results = [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor] #parsing through query
     return jsonify(results)
 
-#endpoint to get a senshi with a given id
-@app.route("/senshi/<int:id>", methods=['GET'])
-def find_senshi(id):
+#endpoint to get a superheroes with a given name
+@app.route("/superheroes/name/<string:name>", methods=['GET'])
+def find_superhero_via_name(name):
     """
         returns data of a senshi with a given id
     """
     conn = db_connection.connect()
-    query = conn.execute("SELECT * FROM Senshi WHERE SenshiId = %d" %int(id))
+    sql = "SELECT * FROM superheroes WHERE name = '%s'" %(name)
+    query = conn.execute(sql)
+    results = [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
+    return jsonify(results)
+
+#endpoint to get a superheroes with a given alias
+@app.route("/superheroes/alias/<string:alias>", methods=['GET'])
+def find_superhero_via_alias(alias):
+    """
+        returns data of a superhero with a given 
+    """
+    conn = db_connection.connect()
+    sql = "SELECT * FROM superheroes WHERE name = '%s'" %(alias)
+    query = conn.execute(sql)
+    results = [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
+    return jsonify(results)
+
+#endpoint to get all superheroes of a species
+@app.route("/superheroes/species/<string:species>", methods=['GET'])
+def get_all_species(species):
+    """
+        returns data of a superheroes of a given species
+    """
+    conn = db_connection.connect()
+    sql = "SELECT * FROM superheroes WHERE species = '%s'" %(species)
+    query = conn.execute(sql)
     results = [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
     return jsonify(results)
