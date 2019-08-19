7b5036893f8bbc9a01bae9ccaa575366217125e0
momolin78 <momolin78@gmail.com>
2019-08-19 15:33:10 -0400

    adding test cases for characters and locations

--- project/data/dataload.py
+++ project/data/dataload.py
@@ -1,10 +1,13 @@
 import json
+import os
 from collections import OrderedDict
 
 def load_characters():
-    with open('./data/charactersdata.json', 'r') as f:
-        return json.load(f, object_pairs_hook=OrderedDict)
+        fname = os.path.join(os.path.dirname(__file__), 'charactersdata.json')
+        with open(fname, 'r') as f:
+                return json.load(f, object_pairs_hook=OrderedDict)
 
 def load_locations():
-        with open ('./data/locationsdata.json', 'r') as f:
+        fname = os.path.join(os.path.dirname(__file__), 'locationsdata.json')
+        with open (fname, 'r') as f:
                 return json.load(f, object_pairs_hook=OrderedDict)
--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,6 @@
 import pytest
+import json
 from flask import Flask
-import sys, os
-sys.path.insert(0, os.path.dirname(__file__))
 from project import *
 
 @pytest.fixture
@@ -20,3 +19,15 @@ def test_about_page(client):
 def test_docs_page(client):
     response = client.get("/docs")
     assert response.status_code == 200
+
+def test_all_characters_api(client):
+    response = client.get("/characters")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert len(data) == 19
+
+def test_all_locations_api(client):
+    response = client.get("/locations")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert len(data) == 4
